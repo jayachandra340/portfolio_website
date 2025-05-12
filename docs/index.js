30e804b19e961bbc5ceca8b52658a94a0eac802e
jayachandra340 <jayachandrakosetti@gmail.com>
2025-05-12 10:29:41 +0530

    updated

---
+++ docs/index.js
@@ -0,0 +1,32 @@
+function Home() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // smooth scrolling
+    });
+  }
+  function About() {
+    window.scrollTo({
+      top: 730, // pixels from the top
+      behavior: "smooth"
+    });
+  }
+  function Skills(){
+    window.scrollTo({
+      top: 1400, 
+      behavior: "smooth"
+    });
+  }
+
+  function Projects(){
+    window.scrollTo({
+      top: 2130, 
+      behavior: "smooth"
+    });
+  }
+
+  function Contact(){
+    window.scrollTo({
+      top: 2900, 
+      behavior: "smooth"
+    });
+  }
